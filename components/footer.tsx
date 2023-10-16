fa8e023f9f9bd288bf370b67069816ad1c3dc362
dejvidEm <bithug10@gmail.com>
2023-10-16 13:46:49 +0200

    edited footer and header

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <footer className='mb-6 px-4 text-center text-gray-600'>
         <small className='text-xs'>
-            &copy; 2023 Dávid Mikuláš. All rights reserved <br/>
-            Built with React, Next.js, Typescript, Node.js, Tailwind CSS, Framer Motion and love.
+            2023<br/>
+            Built with React, Next.js, Typescript, Node.js, Tailwind CSS and Framer Motion.
         </small>
     </footer>
   )
