d74226f9b6b8494bbf8bb6ad05a1e0c949f0bf43
dejvidEm <bithug10@gmail.com>
2023-10-16 14:02:50 +0200

    removed about me

--- lib/data.ts
+++ lib/data.ts
@@ -15,10 +15,10 @@ export const links = [
     name: "Home",
     hash: "#home",
   },
-  {
+/* {
     name: "About",
     hash: "#about",
-  },
+  },*/
   {
     name: "Projects",
     hash: "#projects",
