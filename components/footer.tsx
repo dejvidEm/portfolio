bd54d52e9b6181c6ba8d8c6a2ebf4dcab9a99cc2
dejvidEm <120370347+dejvidEm@users.noreply.github.com>
2024-10-05 10:06:24 +0200

    Update footer.tsx to

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className='mb-6 px-4 text-center text-gray-600'>
         <small className='text-xs'>
-            2023<br/>
+            2024<br/>
             Built with React, Next.js, Typescript, Node.js, Tailwind CSS and Framer Motion.
         </small>
     </footer>
