aca360a3e31429252c787c17bdaf45b7f6bc311f
dejvidEm <bithug10@gmail.com>
2024-11-20 00:48:49 +0100

    update of experience

--- components/intro.tsx
+++ components/intro.tsx
@@ -21,7 +21,7 @@ export default function Intro() {
   useEffect(() => {
     if (el.current) {
       const options = {
-        strings: ["Junior Full-Stack developer", "Caffeine addict"],
+        strings: ["Full-Stack App developer", "Caffeine addict"],
         typeSpeed: 40,
         backSpeed: 40,
         backDelay: 650,
@@ -67,7 +67,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100}}
         animate={{ opacity: 1, y: 0 }}>
           <p className='italic font-light text-sm sm:text-base my-10'>„ Be the change that you wish to see in the world. "</p>
-            <span className='font-bold'>Hello</span>, My name is <span className='font-bold'>David</span> and I am a tech geek based in Slovakia. <br /> I have a <span className='font-bold'>2 years</span> of experience in tech industry<br/><br/><span className='font-bold text-gray-600 dark:text-slate-300'>I'm <span ref={el}></span></span>
+            <span className='font-bold'>Hello</span>, My name is <span className='font-bold'>David</span> and I am a tech geek based in Slovakia. <br /> I have a <span className='font-bold'>4 years</span> of experience in tech industry<br/><br/><span className='font-bold text-gray-600 dark:text-slate-300'>I'm <span ref={el}></span></span>
         </motion.h1>
         <motion.div className='flex flex-col sm:flex-col items-center justify-center gap-5 px-0 text-md sm:text-lg font-medium'
         initial={{ opacity: 0, y: 100}}
